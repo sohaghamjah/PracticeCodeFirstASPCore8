c114ec5182d29815cc0664d8b9ed6805080cb8cb
sohaghamjah <aymansohag.contact@gmail.com>
2025-01-09 12:41:56 +0600

    update and delete

--- obj/Release/net8.0/CodeFirstASPCore8.AssemblyInfo.cs
+++ obj/Release/net8.0/CodeFirstASPCore8.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeFirstASPCore8")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17c771c650b51effda10c8fdbe669b5ea01a545")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeFirstASPCore8")]
